6b9603b3c752889a13c68ee9e591d21edd51d9fa
writchie3 <writchieiii@gmail.com>
2022-02-14 19:46:48 +0000

    OAuth Implemented for all members

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 
 Rails.application.routes.draw do
+  resources :members, :lineages, :events
   devise_for :admins, controllers: { omniauth_callbacks: 'admins/omniauth_callbacks' }
   devise_scope :admin do
     get 'admins/sign_in', to: 'admins/sessions#new', as: :new_admin_session
     get 'admins/sign_out', to: 'admins/sessions#destroy', as: :destroy_admin_session
+    get "/members/:id" , to: "members#show"
+    get "/lineages/:id" , to: "lineages#show"
+    root "dashboards#show"
   end
-  root "members#index"
-  
-  get "/members/:id" , to: "members#show"
-  get "/lineages/:id" , to: "lineages#show"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
