c01e90c26fe828c88e5554f4ce56ef184469b78f
root <root@localhost>
2022-02-25 15:44:49 +0000

    Made changes to the event page. Further changes (to align with the UI sketch) will be made at a later time.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -70,6 +70,6 @@ class EventsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def event_params
-      params.require(:event).permit(:eventID, :name, :location, :start_time, :end_time)
+      params.require(:event).permit(:name, :description, :location, :start_time, :end_time)
     end
 end
