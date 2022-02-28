755e113c3c977b80f69d645b0dbce32350c4ce99
writchie3 <writchieiii@gmail.com>
2022-02-28 02:27:15 +0000

    Changed route from admin/sign_in to sign_in and admin/sign_out to sign_out. Updated routes to only include tamu emails

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   resources :members, :lineages, :events
   devise_for :admins, controllers: { omniauth_callbacks: 'admins/omniauth_callbacks' }
   devise_scope :admin do
-    get 'admins/sign_in', to: 'admins/sessions#new', as: :new_admin_session
-    get 'admins/sign_out', to: 'admins/sessions#destroy', as: :destroy_admin_session
+    get '/sign_in', to: 'admins/sessions#new', as: :new_admin_session
+    get '/sign_out', to: 'admins/sessions#destroy', as: :destroy_admin_session
     get "/members/:id" , to: "members#show"
     get "/lineages/:id" , to: "lineages#show"
     root "dashboards#show"
