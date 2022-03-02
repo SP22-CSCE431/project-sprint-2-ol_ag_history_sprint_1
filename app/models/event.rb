4d44c6de75c2efb64f48a21793a88642f091727c
AlexTorresD <robincrass@tamu.edu>
2022-03-02 02:22:46 +0000

    Testing Fix + View Cleanup

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
 class Event < ApplicationRecord
-<<<<<<< HEAD
     has_many :attendance
     has_many :due
     validates :name, :location, :time, presence: true
-=======
-  validates :name, :location, :time, presence: true
->>>>>>> 28cd43fb4bdeedd37b8a80e4126aebc3c088890f
 end
