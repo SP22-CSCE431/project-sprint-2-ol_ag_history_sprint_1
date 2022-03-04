a168afa054fbb6aed542fc804ebf6a8b74c3b2c1
akk962 <akk962@tamu.edu>
2022-03-04 08:38:50 -0600

    update testing and member's search

---
+++ spec/attendance/feature/integration_spec.rb
@@ -0,0 +1,93 @@
+# frozen_string_literal: true
+
+# location: spec/feature/integration_spec.rb
+require 'rails_helper'
+
+# OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
+#   :provider => 'twitter',
+#   :uid => '123545'
+#   # etc.
+# })
+
+# before do
+#   Rails.application.env_config["devise.mapping"] = Devise.mappings[:user] # If using Devise
+#   Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:twitter]
+# end
+
+OmniAuth.config.silence_get_warning = true
+RSpec.describe('Authentication', type: :feature) do
+  before do
+    Rails.application.env_config['devise.mapping'] = Devise.mappings[:admin]
+    Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google_user]
+    # visit new_admin_session_path click_on "Sign in with Google"
+    visit admin_google_oauth2_omniauth_authorize_path
+    # Permission.create!(description: 'admin') if Permission.where(description: 'admin').first.nil?
+    # unless Admin.where(email: 'userdoe@example.com').first.nil? == false
+    #   Admin.create!(email: 'userdoe@example.com', full_name: 'User Doe', uid: '123456789', avatar_url: 'https://lh3.googleusercontent.com/url/photo.jpg')
+    # end
+  end
+
+  # Attendance Test
+
+  describe 'rsvped and attended', type: :feature do
+    it 'valid inputs' do
+      testMember1 = Member.create!(fname: "John", lname: "Henry", email: "JohnHenry@email.com")
+      testEvent1 = Event.create!(name: "Funeral", location: "Church", start_time: "03/03/2022 10:00PM", end_time: "03/03/2022 11:00PM", description: "N/A")
+      visit new_attendance_path
+      select "John", :from => "Member"
+      select "Funeral", :from => "Event"
+      check 'Rsvp'
+      check 'Attended'
+      click_on "Create Attendance"
+      visit attendances_path
+      expect(page).to have_content('John')
+      expect(page).to have_content('Funeral')
+      expect(page).to have_content('true')
+      expect(page).to have_content('true')
+    end
+  end
+
+  describe 'no rsvp and attended', type: :feature do
+    it 'valid inputs' do
+      testMember1 = Member.create!(fname: "John", lname: "Henry", email: "JohnHenry@email.com")
+      testEvent1 = Event.create!(name: "Funeral", location: "Church", start_time: "03/03/2022 10:00PM", end_time: "03/03/2022 11:00PM", description: "N/A")
+      visit new_attendance_path
+      select "John", :from => "Member"
+      select "Funeral", :from => "Event"
+      check 'Attended'
+      click_on "Create Attendance"
+      visit attendances_path
+      expect(page).to have_content('John')
+      expect(page).to have_content('Funeral')
+      expect(page).to have_content('false')
+      expect(page).to have_content('true')
+    end
+  end
+
+  describe 'Member Name link', type: :feature do
+    it 'valid inputs' do
+      testMember1 = Member.create!(fname: "John", lname: "Henry", email: "JohnHenry@email.com")
+      testMember2 = Member.create!(fname: "Jane", lname: "Doe", email: "JaneDoe@email.com")
+      testEvent1 = Event.create!(name: "Funeral", location: "Church", start_time: "03/03/2022 10:00PM", end_time: "03/03/2022 11:00PM", description: "N/A")
+      testEvent2 = Event.create!(name: "Party", location: "MSC", start_time: "03/03/2022 10:00PM", end_time: "03/03/2022 11:00PM", description: "N/A")
+      visit new_attendance_path
+      select "John", :from => "Member"
+      select "Funeral", :from => "Event"
+      check 'Rsvp'
+      check 'Attended'
+      click_on "Create Attendance"
+      visit attendances_path
+      visit new_attendance_path
+      select "Jane", :from => "Member"
+      select "Party", :from => "Event"
+      check 'Rsvp'
+      check 'Attended'
+      click_on "Create Attendance"
+      visit attendances_path
+      click_link "Member Name"
+      expect(page).to have_content('John')
+      expect(page).to have_content('Jane')
+    end
+  end
+
+end
---
+++ spec/due/feature/integration_spec.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+
+# location: spec/feature/integration_spec.rb
+require 'rails_helper'
+
+# OmniAuth.config.mock_auth[:twitter] = OmniAuth::AuthHash.new({
+#   :provider => 'twitter',
+#   :uid => '123545'
+#   # etc.
+# })
+
+# before do
+#   Rails.application.env_config["devise.mapping"] = Devise.mappings[:user] # If using Devise
+#   Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:twitter]
+# end
+
+OmniAuth.config.silence_get_warning = true
+RSpec.describe('Authentication', type: :feature) do
+  before do
+    Rails.application.env_config['devise.mapping'] = Devise.mappings[:admin]
+    Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google_user]
+    # visit new_admin_session_path click_on "Sign in with Google"
+    visit admin_google_oauth2_omniauth_authorize_path
+    # Permission.create!(description: 'admin') if Permission.where(description: 'admin').first.nil?
+    # unless Admin.where(email: 'userdoe@example.com').first.nil? == false
+    #   Admin.create!(email: 'userdoe@example.com', full_name: 'User Doe', uid: '123456789', avatar_url: 'https://lh3.googleusercontent.com/url/photo.jpg')
+    # end
+  end
+
+  # Due Test
+
+  describe 'paid', type: :feature do
+    it 'valid inputs' do
+      testMember1 = Member.create!(fname: 'John', lname: 'Henry', email: 'JohnHenry@email.com')
+      testEvent1 = Event.create!(name: "Funeral", location: "Church", start_time: "03/03/2022 10:00PM", end_time: "03/03/2022 11:00PM", description: "N/A")
+      visit new_due_path
+      select "John", :from => "Member"
+      select "Funeral", :from => "Event"
+      fill_in 'Dueamount',  with: 120
+      check 'Paid'
+      click_on "Create Due"
+      visit dues_path
+      expect(page).to have_content('John')
+      expect(page).to have_content('Funeral')
+      expect(page).to have_content('120')
+      expect(page).to have_content('true')
+    end
+  end
+
+  describe 'not paid', type: :feature do
+    it 'valid inputs' do
+      testMember1 = Member.create!(fname: 'John', lname: 'Henry', email: 'JohnHenry@email.com')
+      testEvent1 = Event.create!(name: "Funeral", location: "Church", start_time: "03/03/2022 10:00PM", end_time: "03/03/2022 11:00PM", description: "N/A")
+      visit new_due_path
+      select "John", :from => "Member"
+      select "Funeral", :from => "Event"
+      fill_in 'Dueamount',  with: 120
+      click_on "Create Due"
+      visit dues_path
+      expect(page).to have_content('John')
+      expect(page).to have_content('Funeral')
+      expect(page).to have_content('120')
+      expect(page).to have_content('false')
+    end
+  end
+end
