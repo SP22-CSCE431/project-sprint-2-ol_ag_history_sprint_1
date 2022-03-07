39ed4d34111b8409204de490256199c95ab3700b
AlexTorresD <robincrass@tamu.edu>
2022-03-07 04:33:01 +0000

    User View + Edit lock + Seed Fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-Member.create!([
-  {fname: "Walter", lname: "Ritchie", joinDate: "2022-03-04", email: "writchie3@tamu.edu", phoneNumber: "2148936415", city: "College Station", admin: true, major: "Computer Science", active: true, gradDate: "2022-03-04"},
-  {fname: "CJ", lname: "Sewell", joinDate: nil, email: "c.j.sewell07@tamu.edu", phoneNumber: "", city: "", admin: true, major: "Computer Engineering", active: true, gradDate: nil},
-  {fname: "Anna", lname: "Kolodziejcyk", joinDate: nil, email: "akk962@tamu.edu", phoneNumber: "", city: "", admin: true, major: "", active: true, gradDate: nil},
-  {fname: "Alex", lname: "Torres", joinDate: nil, email: "robincrass@tamu.edu", phoneNumber: "", city: "", admin: true, major: "", active: false, gradDate: nil},
-  {fname: "Andrew", lname: "Arzola", joinDate: nil, email: "andrew.j.arzola@tamu.edu", phoneNumber: "", city: "", admin: true, major: "", active: false, gradDate: nil}
-])
+if Member.count == 0
+  Member.create!([
+    {fname: "Walter", lname: "Ritchie", joinDate: "2022-03-04", email: "writchie3@tamu.edu", phoneNumber: "2148936415", city: "College Station", admin: true, major: "Computer Science", active: true, gradDate: "2022-03-04"},
+    {fname: "CJ", lname: "Sewell", joinDate: nil, email: "c.j.sewell07@tamu.edu", phoneNumber: "", city: "", admin: true, major: "Computer Engineering", active: true, gradDate: nil},
+    {fname: "Anna", lname: "Kolodziejcyk", joinDate: nil, email: "akk962@tamu.edu", phoneNumber: "", city: "", admin: true, major: "", active: true, gradDate: nil},
+    {fname: "Alex", lname: "Torres", joinDate: nil, email: "robincrass@tamu.edu", phoneNumber: "", city: "", admin: true, major: "", active: false, gradDate: nil},
+    {fname: "Andrew", lname: "Arzola", joinDate: nil, email: "andrew.j.arzola@tamu.edu", phoneNumber: "", city: "", admin: true, major: "", active: false, gradDate: nil}
+  ])
+end
