1b104a9f21454e0c0ea06e5dc2501af7a0221b96
AndrewArzola <andrew.j.arzola@gmail.com>
2022-03-06 21:38:35 -0600

    Fixed \r issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+Admin.create!([
+  {email: "writchie3@tamu.edu", full_name: "Walter Ritchie", uid: "106113943716370654381", avatar_url: "https://lh3.googleusercontent.com/a/AATXAJxn3RlhpP0rCiIfsefuC00GQ7YrKWfDr1Nw7t5v=s96-c"}
+])
 Member.create!([
   {fname: "Walter", lname: "Ritchie", joinDate: "2022-03-04", email: "writchie3@tamu.edu", phoneNumber: "2148936415", city: "College Station", admin: true, major: "Computer Science", active: true, gradDate: "2022-03-04"},
   {fname: "CJ", lname: "Sewell", joinDate: nil, email: "c.j.sewell07@tamu.edu", phoneNumber: "", city: "", admin: true, major: "Computer Engineering", active: true, gradDate: nil},
